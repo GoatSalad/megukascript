99cfee43122bcd867c02ee39db812425d01dfdfb
boddysh@gmail.com <boddysh@gmail.com>
2018-01-20 02:50:40 -0300

    Update version number (baka)

--- meguca.user.js
+++ meguca.user.js
@@ -3,7 +3,7 @@
 // @namespace   megucasoft
 // @description Does a lot of stuff
 // @include     https://meguca.org/*
-// @version     1.3.3
+// @version     1.4
 // @author      medukasthegucas
 // @grant       none
 // ==/UserScript==
