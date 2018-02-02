b0346f3ede7361f20b5dc18e5a3977abc96800ad
boddysh@gmail.com <boddysh@gmail.com>
2018-02-02 00:45:25 -0300

    update version tehe pero

--- meguca.user.js
+++ meguca.user.js
@@ -3,7 +3,7 @@
 // @namespace   megucasoft
 // @description Does a lot of stuff
 // @include     https://meguca.org/*
-// @version     1.8.1
+// @version     1.8.2
 // @author      medukasthegucas
 // @grant       none
 // ==/UserScript==
