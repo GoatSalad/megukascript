7118e92e7c2ef4af2394885ce22d2524960f8ea8
boddysh@gmail.com <boddysh@gmail.com>
2018-03-22 20:33:02 -0300

    I will not be the father of death

--- meguca.user.js
+++ meguca.user.js
@@ -4,7 +4,7 @@
 // @description Does a lot of stuff
 // @include     https://meguca.org/*
 // @connect     meguca.org
-// @version     2.5.7
+// @version     2.5.8
 // @author      medukasthegucas
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
@@ -21,8 +21,6 @@
                           ["dumbblanc", "dumb blancposters, not cute"],
                           ["sharesOption", "Shares Formatting"],
                           ["screamingPosters", "Vibrate screaming posts"],
-                          ["sekritPosting", "Secret Posting"],
-                          ["imgsekritPosting", "Image Secret Posting<br><br>(Check off the following option if you have drag and drop problems)"],
                           ["enablemegucaplayer","Enable music player"],
                           ["megucaplayerOption", "Show music player<br>"],
                           ["imagePaste", "Upload pasted images"],
@@ -32,6 +30,7 @@
                           ["cancelposters", "Dumb cancelposters"]];
     const nipponeseIndex = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", 
                             "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんゃゅょ心無日口二手山木糸羽雨辵水金色何"];
+
     // The current settings (will be loaded before other methods are called)
     var currentlyEnabledOptions = new Set();
     // Add custom options here if needed
@@ -43,16 +42,6 @@
     // For most new features, you'll want to put a call to your function in this function
     // This will be called multiple times per post, so handlers should be idempotent
     function handlePost(post) {
-        if (currentlyEnabledOptions.has("sekritPosting")) {
-            var secret = findMultipleShitFromAString(post.innerHTML, /<code class=\"code-tag\"><\/code><del>([^#<>\[\]]*)<\/del><code class=\"code-tag\"><\/code>/g);
-            for (var j = secret.length - 1; j >= 0; j--) {
-                parseSecretPost(post, secret[j]);
-            }
-            var secretQuote = findMultipleShitFromAString(post.innerHTML, /[ >]󠁂&gt;󠁂&gt;([\d]+)(?:[ <]+)/g);
-            for (var j = secretQuote.length - 1; j >= 0; j--) {
-                parseSecretQuote(post, secretQuote[j]);
-            }
-        }
         if (currentlyEnabledOptions.has("sharesOption")) {
             var shares = findMultipleShitFromAString(post.innerHTML, /\[([^#\]\[]*)\] <strong( class=\"\w+\")?>#(\d+)d(\d+) \(([\d +]* )*= (?:\d+)\)<\/strong>/g);
             for (var j = shares.length - 1; j >= Math.max(0,shares.length-4); j--) {
@@ -126,24 +115,9 @@
         // Linking to github
         new_cont += "<br><a href=\"https://github.com/GoatSalad/megukascript/blob/master/README.md\" target=\"_blank\">How do I use this?</a>";
 
-        var new_sekrit_cont = "<div data-id=\"6\">";
-
-        // hidetext encode
-        new_sekrit_cont += "<input type=\"textbox\" name=hidetext id=hidetext> <label for=hidetext>Encode Text</label> <button type=\"button\" id=\"secretButton\">Convert & input</button><br>";
-
-        // image for secret message
-        new_sekrit_cont += "<input name=\"secret_image\" id=\"secret_image\" type=\"file\">";
-
-        // Another link to github
-        new_sekrit_cont += "<br><a href=\"https://github.com/GoatSalad/megukascript/blob/master/README.md\" target=\"_blank\">How do I use this?</a>";
-
-        // Secret Encoding tab
-        var new_sekrit_butt = "<a class=\"tab-link\" data-id=\"6\">Secret Encoding</a>";
-
         new_cont += "</div>";
-        new_sekrit_cont += "</div>";
-        tab_butts.innerHTML += new_butt + new_sekrit_butt;
-        tab_cont.innerHTML += new_cont + new_sekrit_cont;
+        tab_butts.innerHTML += new_butt;
+        tab_cont.innerHTML += new_cont;
 
         for (var i = 0; i < onOffOptions.length; i++) {
             var id = onOffOptions[i][0];
@@ -191,82 +165,9 @@
             localStorage.setItem(this.id, (this.value > 60) ? 60 : this.value);
         };
 
-        document.querySelector("#hidetext").addEventListener("keyup", function(event) {
-            if(event.key !== "Enter") return; // Use `.key` instead.
-            document.querySelector("#secretButton").click(); // Things you want to do.
-            event.preventDefault(); // No need to `return false;`.
-        });
-
         document.getElementById("steal_filetypes").value = defaultFiletypes;
         document.getElementById("stealButton").onclick = function() {
             downloadAll();
-        };
-
-        document.getElementById("secretButton").onclick = function() {
-            var fileInput = document.getElementById("secret_image");
-            if (document.getElementById('text-input')!=null) {
-                if (fileInput.files.length == 0) {
-                    // text only
-                    var text = btoa(unescape(encodeURIComponent(document.getElementById('hidetext').value)));
-                    document.getElementById('hidetext').value='';
-                    for (var j = 0; j < nipponeseIndex[0].length; j++) {
-                        if (text.indexOf(nipponeseIndex[0][j]) != -1) 
-                            text = nipponeseIndex[0][j] == "/" || nipponeseIndex[0][j] == "+" ? text.replace(new RegExp("\\" + nipponeseIndex[0][j], 'g'), nipponeseIndex[1][j]) : text.replace(new RegExp(nipponeseIndex[0][j], 'g'), nipponeseIndex[1][j]);
-                    }
-                    document.getElementById('text-input').value = document.getElementById('text-input').value.substring(0,document.getElementById('text-input').selectionStart) + '````**' + text + '**````\n' + document.getElementById('text-input').value.substring(document.getElementById('text-input').selectionEnd);
-                    var evt = document.createEvent('HTMLEvents');evt.initEvent('input', false, true);document.getElementById('text-input').dispatchEvent(evt);
-                } else {
-                    // encode text in an image
-                    var te = new TextEncoder();
-                    var hiddenText = document.getElementById('hidetext').value;
-                    if (te.encode(hiddenText).length > 999) {
-                        alert("secret text too long ;_;");
-                        return;
-                    }
-                    var len = te.encode(hiddenText).length.toString();
-                    if (len.length < 3)
-                        len = "0" + len;
-                    if (len.length < 3)
-                        len = "0" + len;
-
-                    hiddenText += len;
-                    hiddenText += "secret";
-                    var file = fileInput.files[0];
-                    var fr = new FileReader();
-                    fr.onload = function() {
-                        var buffer = this.result;
-                        var newfile = new File([buffer, te.encode(hiddenText)], file.name);
-                        var possibleInputs = document.getElementsByName("image");
-                        var realInput;
-                        for (var i = 0; i < possibleInputs.length; i++) {
-                            if (possibleInputs[i].offsetParent != null) {
-                                realInput = possibleInputs[i];
-                                break;
-                            }
-                        }
-                        // weird hacks to set our modified file
-                        // You can't set the files in a file input in javascript
-                        // But meguca's code has a reference to the file input
-                        // So fuck up the input object, then the browser will let us set `files` on it
-                        // And put back some functions which other parts of meguca's code needs
-                        var obj = new Object;
-                        var oldFns = realInput.__proto__;
-                        realInput.__proto__ = obj.__proto__;
-                        realInput.files = [newfile];
-                        realInput.style = { display : "block" };
-                        realInput.remove = oldFns.remove;
-                        realInput.matches = oldFns.matches;
-                        var evt = document.createEvent('HTMLEvents');
-                        evt.initEvent('change', false, true);
-                        oldFns.dispatchEvent.call(realInput, evt);
-
-                        // clean up
-                        fileInput.value = "";
-                        document.getElementById('hidetext').value='';
-                    };
-                    fr.readAsArrayBuffer(file);
-                }
-            }
         };
     }
 
@@ -415,38 +316,6 @@
         }
 
         post.innerHTML = before + newInner + retreivedRoll + after;
-    }
-
-    function parseSecretPost(post, secret) {
-        var text = secret[1];
-        var before = post.innerHTML.substring(0, secret.index);
-        var after = post.innerHTML.substring(secret.index + secret[0].length);
-
-        for (var j = 0; j < nipponeseIndex[0].length; j++) {
-            text = text.replace(new RegExp(nipponeseIndex[1][j], 'g'), nipponeseIndex[0][j]);
-        }
-
-        var decodedMessage = "";
-        try {
-            decodedMessage = decodeURIComponent(escape(atob(text)));
-        } catch (e) {
-            return;
-        }
-        decodedMessage = decodedMessage.replace(new RegExp("<", 'g'), "<󠁂");
-        decodedMessage = decodedMessage.replace(new RegExp(">", 'g'), "󠁂>");
-        post.innerHTML = before + "<h class=\"sekrit_text\">" + decodedMessage + "</h>" + after;
-    }
-
-    function parseSecretQuote(post, secretQuote) {
-        var quote = secretQuote[1];
-        var before2 = post.innerHTML.substring(0, secretQuote.index);
-        var after2 = post.innerHTML.substring(secretQuote.index + secretQuote[0].length);
-        if (secretQuote[0].substring(secretQuote[0].length-1) == "<" || secretQuote[0].substring(secretQuote[0].length-1) == " ") {
-            after2 = secretQuote[0].substring(secretQuote[0].length-1) + after2;
-            secretQuote[0] = secretQuote[0].substring(0,secretQuote[0].length-1);
-        }
-        quote = "<a class=\"post-link\" data-id=\"" + quote + "\" href=\"#p" + quote + "\">&gt;&gt;" + quote + "</a><a class=\"hash-link\" href=\"#p" + quote + "\"> #</a>";
-        post.innerHTML = before2 + " </h>" + quote + after2; //"<h class=sekrit_text> " + 
     }
 
     function parseShares(post, shares) {
@@ -618,47 +487,6 @@
 
         // pass in the target node, as well as the observer options
         observer.observe(thread, config);
-
-        if (currentlyEnabledOptions.has("imgsekritPosting")) {
-            var parsedImages = {}; // cache results so we don't re-parse images
-
-            var secretConfig = { childList: true };
-            var secretObserver = new MutationObserver(function(mutations) {
-                var mutation = mutations[0]; // only 1 thing will be hovered at a time
-                if (mutation.addedNodes.length > 0) {
-                    var img = mutation.addedNodes[0];
-                    if (img.nodeName != "IMG")
-                        return; // hovering over a post, not an image
-                    if (parsedImages[img.src] !== undefined) {
-                        // we've already parsed (or are parsing) this image
-                        // call addMessageToPost again in case the image was reposed in a new post
-                        if (parsedImages[img.src] !== null) {
-                            addMessageToPost(img, parsedImages[img.src]);
-                        }
-                    } else {
-                        img.onload = function() {
-                            // https://stackoverflow.com/questions/934012/get-image-data-in-javascript/42916772#42916772
-                            // it isn't possible to get the bytes of an existing image, so we need to request it again
-                            // wait until the image has loaded, so it will hopefully come from the cache
-                            // (in my testing, FF seems to always reload it over the network...)
-                            GM_xmlhttpRequest({
-                                method: "GET",
-                                url: img.src,
-                                responseType: 'blob',
-                                onload: function(response) {
-                                    // max message length is 999 bytes + 9 byte header
-                                    var str = response.responseText.substring(response.responseText.length-1008, response.responseText.length);
-                                    var msg = parseSecretImage(img, str);
-                                    parsedImages[img.src] = msg;
-                                }
-                            });
-                        };
-                        parsedImages[img.src] = null; // set to null for now, will be filled in if there's a message
-                    }
-                }
-            });
-            secretObserver.observe(document.getElementById("hover-overlay"), secretConfig);
-        }
     }
 
     function addFormatButton(post) {
@@ -689,38 +517,6 @@
         // pick a random format and add it to both sides of the word
         var format = ["~~","**","__","``"][Math.floor(Math.random()*4)];
         return format + s + format;
-    }
-
-    function parseSecretImage(img, str) {
-        // the message is added to the end of the image
-        // image bytes
-        // text of message
-        // length of message
-        // "secret"
-
-        // check if this contains a secret message
-        var header = str.substring(str.length - 6, str.length);
-        if (header == "secret") {
-            // the next three characters represent the length
-            var length = str.substring(str.length - 9, str.length - 6);
-            length = parseInt(length, 10);
-            if (isNaN(length)) {
-                return null;
-            }
-
-            // convert the rest of the string into an arraybuffer
-            var buf = new ArrayBuffer(str.length - 9);
-            var bufView = new Uint8Array(buf);
-            for (var i = 0; i < str.length - 9; i++) {
-                bufView[i] = str.charCodeAt(i);
-            }
-            // now read the message
-            var td = new TextDecoder();
-            var message = td.decode(buf.slice(buf.byteLength - length, buf.byteLength));
-            addMessageToPost(img, message);
-            return message;
-        }
-        return null;
     }
 
     function addMessageToPost(img, message) {
