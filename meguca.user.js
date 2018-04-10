63c8cd9f061a48949f88b56558209f1d3ee53ea5
dasdgdafg <dasdgdafg@cock.li>
2018-04-10 00:47:02 -0400

    update version number
    this probably counts as a major change

--- meguca.user.js
+++ meguca.user.js
@@ -9,7 +9,7 @@
 // @include     https://chiru.no/*
 // @connect     meguca.org
 // @connect     chiru.no
-// @version     2.7.1
+// @version     3.0.0
 // @author      medukasthegucas
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
